df41517614a04e3cd2e9c81c25b50379a860f18b
yash <yashsaundalkar0@gmail.com>
2023-04-20 11:55:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
+let menu=document.querySelector("#menu-bars");
+let navbar=document.querySelector(".navbar");
 
+menu.onclick=()=>{
+    menu.classList.toggle('fa-time');
+    navbar.classList.toggle("active");
+} 
+
+let slideIndex = 0;
+showSlides();
+
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
+    var x = document.getElementById("drop-down-box");
+    if (x.className.indexOf("") == -1) {
+      x.className += " ";
+    } else { 
+      x.className = x.className.replace(" ", "");
     }
   }
